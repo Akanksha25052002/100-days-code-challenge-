b520317a1d9238c3e6fb9fff2357893196156390
Akanksha25052002 <114761053+Akanksha25052002@users.noreply.github.com>
2022-10-11 22:39:37 +0530

    using nested if-else condition

--- DAY 2/nested if-else.java
+++ DAY 2/nested if-else.java
@@ -1,3 +1,24 @@
+/*In the given problem we use if-else condition.
+
+Given an integer,n, perform the following conditional actions:
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+
+Sample Input 0
+3
+Sample Output 0
+Weird
+Sample Input 1
+24
+Sample Output 1
+Not Weird   */
+
+
+
+//code
+
 import java.util.*;
 public class Oddeven{
     public static void main(String[]args){
